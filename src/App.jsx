0ee5120cdd07ec0a7c8babd0a8d1aad0618b386b
purnesh sharma <purneshsharma2020@gmail.com>
2024-09-25 12:37:29 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   let [title,setTitle] = useState('')
 
   let getMovieList=() =>{
+
+    
     let apiUrl;
     if(title!==''){
       apiUrl=`https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${title}`
